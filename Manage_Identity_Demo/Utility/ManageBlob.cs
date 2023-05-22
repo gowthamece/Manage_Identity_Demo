569a60cb92af5e815810175901f8d922582c9f15
Gowtham <gowthamk91@outlook.com>
2023-05-21 22:15:03 -0400

    Add project files.

---
+++ Manage_Identity_Demo/Utility/ManageBlob.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
+using System.Text;
+
+namespace Manage_Identity_Demo.Utility
+{
+    public class ManageBlob
+    {
+        static public async Task UploadBlob(string accountName, string containerName, string blobName, string blobContents)
+        {
+            // Construct the blob container endpoint from the arguments.
+            string containerEndpoint = string.Format("https://{0}.blob.core.windows.net/{1}",
+                                                        accountName,
+                                                        containerName);
+
+            // Get a credential and create a client object for the blob container.
+            BlobContainerClient containerClient = new BlobContainerClient(new Uri(containerEndpoint),
+                                                                            new DefaultAzureCredential());
+
+            try
+            {
+                // Create the container if it does not exist.
+                await containerClient.CreateIfNotExistsAsync();
+
+                // Upload text to a new block blob.
+                byte[] byteArray = Encoding.ASCII.GetBytes(blobContents);
+
+                using (MemoryStream stream = new MemoryStream(byteArray))
+                {
+                    await containerClient.UploadBlobAsync(blobName, stream);
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ Manage_Identity_MVC_Demo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Manage_Identity_MVC_Demo.Models;
+using Manage_Identity_MVC_Demo.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Manage_Identity_MVC_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            await ManageBlob.UploadBlob("mysawebappmi", "mycontainer", "DemoFile", "Hello World");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
